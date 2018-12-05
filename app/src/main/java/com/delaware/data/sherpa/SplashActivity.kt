5cd98d7b18fb11ddfabbbb036a38555a23f96b25
Vineeth Chohan <vineeth.chohan@gmail.com>
2018-12-05 02:17:58 -0500

    Story/Defect: Initializing Project Description: First commit with splash screen and a home activity User: vchohan

--- app/src/main/java/com/delaware/data/sherpa/SplashActivity.kt
+++ app/src/main/java/com/delaware/data/sherpa/SplashActivity.kt
@@ -1,12 +1,55 @@
 package com.delaware.data.sherpa
 
+import android.app.Application
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 
 class SplashActivity : AppCompatActivity() {
+
+    private var delayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUi()
+        }
+    }
+
+    private fun hideSystemUi() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    internal val splashRunnable: Runnable = Runnable {
+        val intent = Intent(applicationContext, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
+
+        //Initialize the Handler
+        delayHandler = Handler()
+
+        //Navigate with delay
+        delayHandler!!.postDelayed(splashRunnable, SPLASH_DELAY)
     }
+
+    override fun onDestroy() {
+        if (delayHandler != null) {
+            delayHandler!!.removeCallbacks(splashRunnable)
+        }
+        super.onDestroy()
+    }
+    
 }
